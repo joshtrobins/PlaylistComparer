9d4450534ce31f3953e115340555300763415e0a
Josh <joshtrobins@gmail.com>
2023-06-11 10:42:26 -0700

    Add directory walking

---
+++ PlaylistAnalyzer.py
@@ -0,0 +1,34 @@
+import os
+
+import PlaylistComparer
+import PlaylistFileReader
+import PlaylistFileWriter
+import PlaylistScraper
+
+
+def analyze(full_path):
+    path, file = os.path.split(full_path)
+    config_path = path + "\\config.txt"
+
+    client_id = ""
+    client_secret = ""
+    username = ""
+    if os.path.exists(config_path):
+        with open(config_path, "r", encoding="utf-8", newline="") as config_file:
+            lines = config_file.readlines()
+            for line in lines:
+                if line.startswith("Client ID:"):
+                    client_id = line[10:].strip()
+                elif line.startswith("Client Secret:"):
+                    client_secret = line[14:].strip()
+                elif line.startswith("Username:"):
+                    username = line[9:].strip()
+    else:
+        client_id = input("Client ID: ")
+        client_secret = input("Client Secret: ")
+        username = input("Username: ")
+
+    new_playlists = PlaylistScraper.scrape(client_id, client_secret, username)
+    old_playlists = PlaylistFileReader.read(full_path)
+    PlaylistFileWriter.write(path, new_playlists)
+    PlaylistComparer.compare(path, old_playlists, new_playlists)
--- Runner.py
+++ Runner.py
@@ -1,33 +1,19 @@
-import os.path
+import datetime
+import os
 
-import PlaylistComparer
-import PlaylistFileReader
-import PlaylistFileWriter
-import PlaylistScraper
+import PlaylistAnalyzer
 
 full_path = input("Path: ").strip("\"")
-path, file = os.path.split(full_path)
-config_path = path + "\\config.txt"
-
-client_id = ""
-client_secret = ""
-username = ""
-if os.path.exists(config_path):
-    with open(config_path, "r", encoding="utf-8", newline="") as config_file:
-        lines = config_file.readlines()
-        for line in lines:
-            if line.startswith("Client ID:"):
-                client_id = line[10:].strip()
-            elif line.startswith("Client Secret:"):
-                client_secret = line[14:].strip()
-            elif line.startswith("Username:"):
-                username = line[9:].strip()
-else:
-    client_id = input("Client ID: ")
-    client_secret = input("Client Secret: ")
-    username = input("Username: ")
-
-new_playlists = PlaylistScraper.scrape(client_id, client_secret, username)
-old_playlists = PlaylistFileReader.read(full_path)
-PlaylistFileWriter.write(path, new_playlists)
-PlaylistComparer.compare(path, old_playlists, new_playlists)
+for directory in os.scandir(full_path):
+    recent_date = datetime.date.min
+    recent_file = ""
+    if directory.is_dir():
+        for file in os.scandir(directory):
+            if file.name.endswith("csv"):
+                date = datetime.datetime.strptime(file.name[18:26], "%m%d%Y").date()
+                if date > recent_date:
+                    recent_date = date
+                    recent_file = file
+        print("Analyzing \"" + directory.name + "\" from archive \"" + str(recent_date) + "\"")
+        PlaylistAnalyzer.analyze(recent_file.path)
+print("Done")
